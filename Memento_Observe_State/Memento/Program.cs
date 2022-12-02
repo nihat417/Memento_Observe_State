1750e18fb4e49dea457b05aed849e6eb3ab8bf2b
Nihat Akremi <nihatakremi@gmail.com>
2022-12-02 18:48:12 +0300

    Добавьте файлы проекта.

---
+++ Memento_Observe_State/Memento/Program.cs
@@ -0,0 +1,78 @@
+﻿public interface IObserver
+{
+    void Update();
+}
+
+
+public class UserInteface : IObserver
+{
+    public void Update()
+    {
+        Console.WriteLine("Interface Updated");
+    }
+}
+public class DifferentUnit : IObserver
+{
+    public void Update()
+    {
+        Console.WriteLine("Relation Updated");
+    }
+}
+
+
+interface IEnemy
+{
+    void Subscribe(IObserver observer);
+    void Unsubscribe(IObserver observer);
+    void Notify();
+}
+
+
+public class Enemy : IEnemy
+{
+    private List<IObserver> observers = new List<IObserver>();
+    private int _health = 10;
+    public int Health
+    {
+        get { return _health; }
+        set
+        {
+            _health = value;
+            Notify();
+        }
+    }
+
+    public void Notify()
+    {
+        observers.ForEach(x => x.Update());
+    }
+
+    public void Subscribe(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void Unsubscribe(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Enemy subject = new Enemy();
+
+        IObserver observer1 = new UserInteface();
+        subject.Subscribe(observer1);
+
+        subject.Subscribe(new DifferentUnit());
+        subject.Health++;
+        Console.WriteLine("--------------------");
+        subject.Unsubscribe(observer1);
+        subject.Health--;
+        Console.ReadLine();
+    }
+}
---
+++ Memento_Observe_State/State/State.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento_Observe_State.State;
+
+interface MobileAlertState
+{
+    void Alert(AlertStateContext ctx);
+}
+
+class AlertStateContext
+{
+    private MobileAlertState currentState;
+
+    public AlertStateContext()
+    {
+        currentState = new Vibration();
+    }
+
+    public void SetState(MobileAlertState state) => currentState = state;
+    public void Alert() => currentState.Alert(this);
+}
+
+class Vibration : MobileAlertState
+{
+    public void Alert(AlertStateContext ctx) => Console.WriteLine("Vibration...");
+}
+
+class Silent : MobileAlertState
+{
+    public void Alert(AlertStateContext ctx) => Console.WriteLine("Silent...");
+}
+
+class Program
+{
+    static void Main()
+    {
+        AlertStateContext alertStateContext = new AlertStateContext();
+        alertStateContext.Alert();
+        alertStateContext.Alert();
+        alertStateContext.Alert();
+        alertStateContext.SetState(new Silent());
+        alertStateContext.Alert();
+    }
+}
